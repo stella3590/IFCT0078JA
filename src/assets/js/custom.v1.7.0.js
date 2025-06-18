af04d974ab28a347fd01c08c00f88137e68b8a87
Stella TF <stella.stf@gmail.com>
2025-06-18 12:49:57 +0200

    UpdateArray

--- src/assets/js/custom.v1.7.0.js
+++ src/assets/js/custom.v1.7.0.js
@@ -1,6 +1,25 @@
-// Funciones de conversión
-function mostrarMensaje(mensaje, tipo = "log") {
-  alert(mensaje);
-  console[tipo](mensaje);
-}
+//Javascript
 
+// Definimos un array con 5 frutas diferentes
+var frutas = ['Cereza', 'Kiwi', 'Melón', 'Uva', 'Papaya'];
+
+// Mostramos todas las frutas usando forEach
+console.log("Frutas actuales:");
+
+// frutas.forEach(function(fruta, index) {
+//   console.log(`${index + 1}. ${fruta}`);
+// // });
+
+frutas.forEach((fruta)  => console.log(`${fruta}`) + "\r\n");
+
+
+
+// Pedimos al usuario una nueva fruta
+var nuevaFruta = prompt("Introduce una nueva fruta:");
+frutas.push(nuevaFruta);
+
+// Mostramos el array actualizado
+console.log("\nFrutas después de añadir la nueva:");
+frutas.forEach(function(fruta, index) {
+  console.log(`${index + 1}. ${fruta}`);
+});
